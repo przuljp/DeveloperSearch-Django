4d11d69526d784accbc97058e5b7035c799c78ee
przuljp <petar.przulj@stu.ibu.edu.ba>
2026-04-27 19:24:57 +0200

    small fix

--- projects/views.py
+++ projects/views.py
@@ -27,7 +27,7 @@ def createProject(request):
             project = form.save(commit=False)
             project.owner = profile
             project.save()
-            return redirect('projects')
+            return redirect('account')
 
     context = {"form": form}
     return render(request, 'projects/project_form.html', context)
@@ -42,7 +42,7 @@ def updateProject(request, pk):
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect('account')
 
     context = {"form": form}
     return render(request, 'projects/project_form.html', context)
